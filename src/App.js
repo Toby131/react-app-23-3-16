3aa948e250fae26bff5889db7a534ba909775544
Toby131 <leehj94131@gmail.com>
2023-03-16 11:59:20 +0900

    upload file

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function Counter(props){
+  console.log('props',props);
+  return <div>
+    <h1>Counter</h1>
+    <button> 🐷 </button> 👉 0
+  </div>
+}
+function App() {
+  return (
+    <div>
+      <Counter title="불면증 카운터" initValue="10"></Counter>
+      <Counter title="입장객 카운터" initValue="20"></Counter>
+    </div>
+  );
+}
+
+export default App;
